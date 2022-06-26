f095fc2e0b5f09ca38f788dac268a148cc88f3fc
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-06-26 21:47:40 +0100

    Render Function

--- Space Invaders/player.lua
+++ Space Invaders/player.lua
@@ -8,3 +8,10 @@ function player:init()
   self.x = virtual_screen / 2 - (self.width / 2)
   self.y = virtual_height / 2 - (self.height + 10)
 end
+
+-- Used to draw the image to the screen when called
+   -- Uses colons to pass in the self parameter
+function player:render()
+  -- Used to draw the image to screen
+  love.graphics.draw(self.image, self.x, self.y)
+end

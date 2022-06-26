999e536ffb92588a84adade9c75642d5b99e950b
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-06-26 22:08:58 +0100

    Class Import

--- Space Invaders/main.lua
+++ Space Invaders/main.lua
@@ -2,7 +2,9 @@
 window_height = 1080;
 window_width = 1920;
 
--- SInce we want to have a more retro aesthetic, we require the "push" library.
+Class = require "class"
+
+-- Since we want to have a more retro aesthetic, we require the "push" library.
    -- This code was forked from the CS:50 Game Dev Harvard Course Pong Repo.
 push = require "push"
 

3593ff713a6104b7e382077235f6645d6e08f953
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-06-26 21:47:53 +0100

    Class Requirement

--- Space Invaders/main.lua
+++ Space Invaders/main.lua
@@ -5,3 +5,6 @@ window_width = 1920;
 -- SInce we want to have a more retro aesthetic, we require the "push" library.
    -- This code was forked from the CS:50 Game Dev Harvard Course Pong Repo.
 push = require "push"
+
+-- Used to import the player class
+player = require "player"

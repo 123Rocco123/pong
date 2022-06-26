d01ec53c45eae4f1b590daa43fbbbe268b6f5435
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-06-26 22:09:57 +0100

    Y-Axis Error Fix

--- Space Invaders/player.lua
+++ Space Invaders/player.lua
@@ -7,8 +7,9 @@ function player:init()
   self.width = self.image.getWidth()
   self.height = self.image.getHeight()
 
+  -- Used to set the position of the player
   self.x = virtual_screen / 2 - (self.width / 2)
-  self.y = virtual_height / 2 - (self.height + 10)
+  self.y = virtual_height - (self.height + 10)
 end
 
 -- Used to draw the image to the screen when called
